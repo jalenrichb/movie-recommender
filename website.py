8bffc9b51cee1240db933a5320f5bbe7b9aa1af4
Jalen Richburg <jalenrichburg@Jalens-MacBook-Air-2.local>
2022-07-30 22:27:35 -0400

    adding initial files

---
+++ website.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/recommendations')
+def recommendations():
+    return render_template("recommendations.html")
+
+@app.route('/list')
+def list():
+    return render_template("list.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form["nm"]
+    else:
+        return render_template('login.html')
+
+@app.route('/<usr>')
+def user(usr):
+    return f'<h1>{usr}</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
