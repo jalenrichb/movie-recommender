acb26b23d945a956d8489e40c7294e9c71e2711d
Jalen Richburg <jalenrichburg@Jalens-MacBook-Air-2.local>
2022-07-31 13:07:23 -0400

    created logout route, added permanent routes

--- website.py
+++ website.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = 'littlebites'
+app.permanent_session_lifetime = timedelta(days=1)
 
 @app.route('/')
 def home():
@@ -17,15 +20,27 @@ def list():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         user = request.form["nm"]
         session["user"] = user
-        return redirect(url_for("user", usr=user))
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template('login.html')
 
-@app.route('/<user>')
+@app.route('/user')
 def user():
-    return f'<h1></h1>'
+    if "user" in session:
+        user = session["user"]
+        return f'<h1>{user}</h1>'
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
